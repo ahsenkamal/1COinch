f6e32a46e5c85af92b791d2e2b9a881ddc5e51ee
Ahsen Kamal <itsahsenkamal@gmail.com>
2025-08-02 19:18:13 +0530

    cleanup
    Signed-off-by: Ahsen Kamal <itsahsenkamal@gmail.com>

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.23",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    },
+    viaIR: true
+  },
+  networks: {
+    sepolia: {
+        url: process.env.ALCHEMY_API_URL,
+        accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
